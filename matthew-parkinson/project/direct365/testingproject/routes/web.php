0468e09d3bdae501e7b7c08aab674444b0e1055d
MattParki <mattparkinson_12@hotmail.com>
2022-02-19 10:30:42 +0000

    setup the route to head towards the product controller when /products is reached

--- matthew-parkinson/project/direct365/testingproject/routes/web.php
+++ matthew-parkinson/project/direct365/testingproject/routes/web.php
@@ -17,3 +17,5 @@ Route::get('/', function () {
 
 
 Route::get('home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('products', [ProductController::class, 'index'])->name('products.index');
